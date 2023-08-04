969974502be5b89bd3b1c2f29d023da83103bb13
Ali Hosseini <ali@ali.de>
2023-08-04 23:48:30 +0000

    docker_hw2.sh --> show statistic for all running nginx containes

--- docker_hw2.sh
+++ docker_hw2.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 
+# create a variable to show 24 hours ago
+time_24_hours_ago=$(date -u -d "24 hours ago" "+%Y-%m-%dT%H:%M:%S")
 
 # numer of running_nginx container
 number_of_nginx=$(docker ps | grep nginx | awk '{print $1}' | wc -l)
 
-# only one nginx container is allowded
+# if there is no enginx container show error and exit
 if [ -z "$number_of_nginx" ]; then
-   echo 'there is no active NGINX container!'
-   exit 1
-elif [ "$number_of_nginx" -gt 1 ]; then
-   echo 'more than one NGINX container.'
+   echo 'there is no running NGINX container!'
    exit 1
 fi
 
-container_id=$(docker ps | grep nginx | awk '{print $1}')
+# if there exist more than one nginx container
+# show no of Ips for every nginx
+for ((i=1; i<=$number_of_nginx; i++)); do
+	
+	# extract container_id and ip
+	container_id=$(docker ps | grep nginx | awk '{print $1}' |	sed -n "${i}p")
+	container_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container_id)
 
-# create a variable to show 24 hours ago
-time_24_hours_ago=$(date -u -d "24 hours ago" "+%Y-%m-%dT%H:%M:%S")
 
-total_count=$(docker logs $container_id | awk '/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/ {print $1}' | sort | uniq | wc -l)
-Last_24H_count=$(docker logs --since $time_24_hours_ago $container_id | awk '/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/ {print $1}' | sort | uniq | wc -l)
+	# extract ip addresses that are connected
+	total_count=$(docker logs $container_id | awk '/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/ {print $1}' | sort | uniq | wc -l)
+	Last_24H_count=$(docker logs --since $time_24_hours_ago $container_id | awk '/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/ {print $1}' | sort | uniq | wc -l)
 
-container_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container_id)
+	# show the results
+	echo   # blank line
 
- 
-echo "Total number of ip addresses that connected to $container_IP: $total_count"
-echo "No of connections to $container_IP in the last 24 hours : $Last_24H_count"
+	echo "container IP: $container_IP"
+	echo "***************************************************"
+	echo "Total number of connected: $total_count"
+	echo "Number of connections in the last 24 hours: $Last_24H_count"
+done
 
