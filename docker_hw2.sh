0eee0f2e2976369502189fc8f28c7db95adf4034
Ali Hosseini <ali@ali.de>
2023-08-04 23:52:25 +0000

    comments modified

--- docker_hw2.sh
+++ docker_hw2.sh
@@ -16,10 +16,9 @@ fi
 # show no of Ips for every nginx
 for ((i=1; i<=$number_of_nginx; i++)); do
 	
-	# extract container_id and ip
-	container_id=$(docker ps | grep nginx | awk '{print $1}' |	sed -n "${i}p")
+	# extract container id and ip
+	container_id=$(docker ps | grep nginx | awk '{print $1}' | sed -n "${i}p")
 	container_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container_id)
-
 
 	# extract ip addresses that are connected
 	total_count=$(docker logs $container_id | awk '/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/ {print $1}' | sort | uniq | wc -l)
