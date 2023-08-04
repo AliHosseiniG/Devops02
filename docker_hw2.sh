f46bf9915f04553a7a70fb096dc4843abdea9def
Ali Hosseini <ali@ali.de>
2023-08-04 21:05:40 +0000

    second homework created!

---
+++ docker_hw2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+# numer of running_nginx container
+number_of_nginx=$(docker ps | grep nginx | awk '{print $1}' | wc -l)
+
+# only one nginx container is allowded
+if [ -z "$number_of_nginx" ]; then
+   echo 'there is no active NGINX container!'
+   exit 1
+elif [ "$number_of_nginx" -gt 1 ]; then
+   echo 'more than one NGINX container.'
+   exit 1
+fi
+
+container_id=$(docker ps | grep nginx | awk '{print $1}')
+
+# create a variable to show 24 hours ago
+time_24_hours_ago=$(date -u -d "24 hours ago" "+%Y-%m-%dT%H:%M:%S")
+
+total_count=$(docker logs $container_id | awk '/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/ {print $1}' | sort | uniq | wc -l)
+Last_24H_count=$(docker logs --since $time_24_hours_ago $container_id | awk '/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/ {print $1}' | sort | uniq | wc -l)
+
+echo "Total number of ip addresses: $total_count"
+echo "No of connections in the last 24 hours: $Last_24H_count"
+
