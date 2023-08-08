133aae1b73d43184bf73f922f6a3910217b0d5e2
mehdimesgar <mehdi.mesgar@gmail.com>
2023-08-08 21:11:26 +0000

    remove more than 12 archive file

--- docker_hw1.sh
+++ docker_hw1.sh
@@ -2,7 +2,7 @@
 
 # Get the hostname of the system
 host_name=$(hostname)
-
+count_limit=12
 # Get the count of active containers using container names
 # container_count=$(docker ps -q | wc -l)
 container_count=$(docker ps --format "table {{.Names}}" | wc -l)
@@ -21,9 +21,28 @@ for ((i=2; i<=$container_count; i++)); do
     # Combine the date/time and hostname
     current_datetime=$(date +"%Y%m%d_%H:%M:%S")
 
-    result="$container_name""_""${current_datetime}""_""${host_name}.tar"
 
+    result="$host_name""_""${current_datetime}""_""${container_name}.tar"
     #export container as a tar file in /tmp/ 
     docker export $container_name >/tmp/$result
 
+
+    # Count the number of exported files
+    count=$(ls -l /tmp/*"$container_name".tar | wc -l )
+
+
+    # If count exceeds the 12, delete the oldest file(s)
+    if (( count > count_limit )); then
+        num_files_to_delete=$(( count - count_limit ))
+
+        oldest_files=$(ls -1t  /tmp/*"$container_name".tar | tail -n "$num_files_to_delete" | cut -d" " -f 10)
+        rm $oldest_files
+    fi
+
+       #Sleep  for ne hour before exporting again
+       sleep 1h
+
+
+
 done
+
